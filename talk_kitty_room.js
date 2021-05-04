08ab12cc6b4c7ffc5379fd384899a1eb2d7ac19a
code-maker-kitty <77761014+code-maker-kitty@users.noreply.github.com>
2021-05-04 08:13:30 -0700

    Add files via upload

--- talk_kitty_room.js
+++ talk_kitty_room.js
@@ -1,14 +1,16 @@
 //ADD YOUR FIREBASE LINKS HERE
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyC56yyIAc9CrRsx34PI8YxTBhQFpnklEgs",
-    authDomain: "kwitter-1ad8a.firebaseapp.com",
-    projectId: "kwitter-1ad8a",
-    storageBucket: "kwitter-1ad8a.appspot.com",
-    messagingSenderId: "420671870446",
-    appId: "1:420671870446:web:5a369647e899bbb7a7aaff"
-  };
-  // Initialize Firebase
+  apiKey: "AIzaSyAKa2YsFatXtfiBqWNqZi40f6LDYcKDCXY",
+  authDomain: "talk-kitty.firebaseapp.com",
+  databaseURL: "https://talk-kitty-default-rtdb.firebaseio.com",
+  projectId: "talk-kitty",
+  storageBucket: "talk-kitty.appspot.com",
+  messagingSenderId: "82377960355",
+  appId: "1:82377960355:web:7e60b54488ca3b41e08816"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
   firebase.initializeApp(firebaseConfig);
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
      Room_names = childKey;
